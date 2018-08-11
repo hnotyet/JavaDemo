7933df93e1455a943ccca1389c6e2426d805184a
yeshengtao <yeshengtao@192.168.31.56>
2018-08-11 22:47:55 +0800

    限流算法

---
+++ src/com/QpsLimit.java
@@ -0,0 +1,34 @@
+package com;
+
+/**
+ * 固定时间窗口 限流
+ * @author yeshengtao
+ *
+ */
+public class QpsLimit {
+
+	private static Long start_time = null;
+	private int count = 0;
+	
+	private int QPS_LIMIT = 100;
+	
+	static {
+		start_time = System.currentTimeMillis();
+	}
+	
+	public synchronized  boolean isOverLimit() {
+		Long now = System.currentTimeMillis(); 
+		if(now-start_time >=1000L) {
+			if(++count > QPS_LIMIT) {
+				return true;
+			}else {
+				start_time = now;
+				count =0;
+			}
+		}else {
+			count++;
+		}
+		return false;
+	}
+	
+}
